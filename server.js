4bf0a315757b5645cd759790aac3bf5d4b690c2d
param <dahalpar@gmail.com>
2019-08-18 02:32:11 -0400

    added some stuff

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'); 
+var path = require('path'); 
+var bodyParser = require('body-parser'); 
+
+var index = require('./routes/index'); 
+var tasks = require('./routes/task'); 
+
+var app = express(); 
+var port = 8000; 
+
+//view engine
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'ejs'); 
+app.engine('html', require('ejs').renderFile); 
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'client'))); 
+
+//Body parser MW
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+app.use('/', index); 
+app.use('/api', tasks); 
+
+app.listen(port, function () {
+    console.log('server started on port' + port);
+}); 
